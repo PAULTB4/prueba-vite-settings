2efc0959d1aec878d308fc32ea0920540573e4ab
PAULTB4 <paul.tarazona@unas.edu.pe>
2026-01-08 10:15:23 -0500

    first commit

---
+++ src/main.js
@@ -0,0 +1,28 @@
+import './style.css'
+import viteLogo from '/vite.svg'
+
+document.querySelector('#app').innerHTML = `
+<h1> Paul marco tb</h1>   
+<form>
+<label>Nombre</label>
+<input type"email">
+<button id="clickMe" type="button" > clickme</button>
+</form>
+
+<div id="answer"> 
+
+</div>
+
+<a href="https://vite.dev" target="_blank">
+      <img src="${viteLogo}" class="logo" alt="Vite logo" />
+    </a>
+`;
+
+let counter = 0;
+const btnClick = document.querySelector("#clickMe")
+const divAnsw = document.querySelector("#answer");
+
+btnClick.addEventListener("click", () => {
+    counter++;
+    divAnsw.innerHTML = counter;
+});
